02a579f8d90e06a1cbb3dfffa1510427e4d96f89
SupahAmbition <tylerbeverley@gmail.com>
2019-12-04 15:42:43 -0600

    created functions for graphs

--- Graphs.py
+++ Graphs.py
@@ -1,24 +1,29 @@
+#!/usr/bin/python3
+
+
 import matplotlib.pyplot as plt
 from lifelines import KaplanMeierFitter
 from lifelines import NelsonAalenFitter
 
+#durations = [11, 74, 71, 76, 28, 92, 89, 48, 90, 39, 63, 36, 54, 64, 34, 73, 94, 37, 56, 76]
+#event_observed = [1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1]
+
+def createSurvivalGraph( durations, event_observed ): 
+    kmf = KaplanMeierFitter()
+    kmf.fit(durations, event_observed)
+    kmf.plot(ci_show=False)
+
+    plt.title("Hard Drive Kaplan Meier Survival Analysis")
+    plt.ylabel("Probability a Hard Drive Survives")
+    plt.show()
 
 
-durations = [11, 74, 71, 76, 28, 92, 89, 48, 90, 39, 63, 36, 54, 64, 34, 73, 94, 37, 56, 76]
-event_observed = [1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1]
 
-kmf = KaplanMeierFitter()
-kmf.fit(durations, event_observed)
-kmf.plot(ci_show=False)
+def createHarzardGraph( durations, event_observed ): 
+    naf = NelsonAalenFitter()
+    naf.fit(durations, event_observed)
+    naf.plot(ci_show=False)
 
-plt.title("Hard Drive Kaplan Meier Survival Analysis")
-plt.ylabel("Probability a Hard Drive Survives")
-plt.show()
-
-naf = NelsonAalenFitter()
-naf.fit(durations, event_observed)
-naf.plot(ci_show=False)
-
-plt.title("Hard Drive Nelson-Aalen Hazard Estimate")
-plt.ylabel("Cumulative Hazard")
-plt.show()
+    plt.title("Hard Drive Nelson-Aalen Hazard Estimate")
+    plt.ylabel("Cumulative Hazard")
+    plt.show()
